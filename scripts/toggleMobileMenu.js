822e90383837849e3193cc280de98dae8640cffb
Kamila Dynysiuk <kamila.dynysiuk@gmail.com>
2024-01-29 08:30:05 +0100

    fix mobile menu, add lazy loading

--- scripts/toggleMobileMenu.js
+++ scripts/toggleMobileMenu.js
@@ -5,13 +5,40 @@ class HamburgerMenu {
     this.hamburgerBtn = document.querySelector('#hamburgerBtn');
     this.menu = document.querySelector('#menu');
     this.body = document.querySelector('body');
+    this.navList = document.querySelector('#navList');
     this.hamburgerBtn.addEventListener('click', this.toggleMenu.bind(this));
+    this.navLinks = document.querySelectorAll('.nav__link');
+    this.navLinks.forEach((link) => {
+      link.addEventListener('click', (e) => this.closeMenu(e, link));
+    });
+
+    window.addEventListener('resize', this.handleResize.bind(this));
   }
 
   toggleMenu() {
     this.hamburgerBtn.classList.toggle('active');
     this.menu.classList.toggle('active');
     this.body.classList.toggle('no-scroll');
+  }
+  closeMenu(e, link) {
+    e.preventDefault();
+    const closestLink = link.closest('#navList');
+    const siblings = closestLink.querySelectorAll('.nav__link');
+    siblings.forEach((el) => {
+      if (el !== closestLink) el.classList.remove('nav__link--active');
+    });
+
+    link.classList.add('nav__link--active');
+    this.toggleMenu();
+    this.body.classList.remove('no-scroll');
+  }
+  handleResize() {
+    const bodyWidth = this.body.getBoundingClientRect().width;
+    if (bodyWidth > 992) {
+      this.hamburgerBtn.classList.remove('active');
+      this.menu.classList.remove('active');
+      this.body.classList.remove('no-scroll');
+    }
   }
 }
 
